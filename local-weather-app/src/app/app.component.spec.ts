1ea2de323eb99e1019b9e6ab0964a51529054600
Nevena Stoeva <nevena.stoeva@gmail.com>
2018-09-29 09:45:45 -0700

    Adds modules to app componen spec

--- local-weather-app/src/app/app.component.spec.ts
+++ local-weather-app/src/app/app.component.spec.ts
@@ -1,16 +1,28 @@
 import { TestBed, async } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CitySearchComponent } from './city-search/city-search.component';
+import { WeatherService } from './weather/weather.service';
+import { WeatherServiceFake } from './weather/weather.service.fake';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports:[HttpClientTestingModule],
+      imports:[
+        HttpClientTestingModule,
+        FormsModule,
+        ReactiveFormsModule,
+      ],
       declarations: [
         AppComponent,
-        CurrentWeatherComponent
+        CurrentWeatherComponent,
+        CitySearchComponent,
       ],
+      providers: [
+        {provide: WeatherService, useClass: WeatherServiceFake}
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -18,15 +30,5 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'local-weather-app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('local-weather-app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('weather');
-  }));
+  
 });
