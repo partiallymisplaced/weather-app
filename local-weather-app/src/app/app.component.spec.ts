e94da118f815ac33ac9afa52068330684687da66
Nev <nevena.stoeva@gmail.com>
2018-09-30 19:32:35 -0700

    Adds tests for city search component

--- local-weather-app/src/app/app.component.spec.ts
+++ local-weather-app/src/app/app.component.spec.ts
@@ -2,14 +2,24 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CitySearchComponent } from './city-search/city-search.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports:[HttpClientTestingModule],
+      imports:[
+        BrowserModule,
+        HttpClientTestingModule,
+        FormsModule,
+        ReactiveFormsModule,      
+
+      ],
       declarations: [
         AppComponent,
-        CurrentWeatherComponent
+        CurrentWeatherComponent,
+        CitySearchComponent
       ],
     }).compileComponents();
   }));
@@ -17,16 +27,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'local-weather-app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('local-weather-app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('weather');
   }));
 });
